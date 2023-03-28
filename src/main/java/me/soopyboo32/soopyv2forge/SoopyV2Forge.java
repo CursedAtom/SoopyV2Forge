04d0c3b14e5cbc4db45f38ca9f9d702529377149
CursedAtom <88254538+CursedAtom@users.noreply.github.com>
2023-03-27 22:34:27 -0400

    Fixed issue with capitalisation

--- src/main/java/me/soopyboo32/soopyv2forge/SoopyV2Forge.java
+++ src/main/java/me/soopyboo32/soopyv2forge/SoopyV2Forge.java
@@ -29,8 +29,8 @@ import java.util.zip.ZipInputStream;
 @Mod(modid = SoopyV2Forge.MODID, version = SoopyV2Forge.VERSION)
 public class SoopyV2Forge
 {
-    public static final String MODID = "SoopyV2Forge";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "soopyv2forge";
+    public static final String VERSION = "1.1";
 
     public static SoopyV2Forge INSTANCE;
 
